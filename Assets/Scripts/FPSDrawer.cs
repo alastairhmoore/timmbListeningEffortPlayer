a794e8d4e22a2ad320fc91f9282281cd6905f889
Tim <timmb@matisse>
2019-08-07 17:28:34 +0100

    Add in FPS counter for video players. Reorganise assets folder

---
+++ Assets/Scripts/FPSDrawer.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using System.Linq;
+
+public class FPSDrawer : MonoBehaviour
+{
+    //public VideoPlayer video0;
+    private TMPro.TextMeshProUGUI Text;
+    private VideoFPSCounter[] videoFPSCounters;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        VideoPlayer[] videoPlayers = FindObjectsOfType<VideoPlayer>();
+        videoFPSCounters = new VideoFPSCounter[videoPlayers.Length];
+        for (int i=0; i<videoPlayers.Length; i++)
+        {
+            videoFPSCounters[i] = videoPlayers[i].GetComponent<VideoFPSCounter>();
+            Debug.Assert(videoFPSCounters[i] != null);
+        }
+
+        Text = GetComponent<TMPro.TextMeshProUGUI>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Text.text = "";
+        for (int i = 0; i < videoFPSCounters.Length; i++)
+        {
+            Text.text += $"video{i}: {videoFPSCounters[i].CurrentFPS:0.} fps\n";
+        }
+    }
+}
---
+++ Assets/Scripts/VideoFPSCounter.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+#pragma warning disable RECS0018 // Comparison of floating point numbers with equality operator
+
+public class VideoFPSCounter : MonoBehaviour
+{
+    VideoPlayer videoPlayer;
+    private float timeOfLastFrame;
+    public float CurrentFPS;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        videoPlayer = GetComponent<VideoPlayer>();
+        videoPlayer.sendFrameReadyEvents = true;
+
+        videoPlayer.frameReady += (VideoPlayer source, long frameIdx) =>
+        {
+            float dt = Time.time - timeOfLastFrame;
+            timeOfLastFrame = Time.time;
+            if (dt==0.0)
+            {
+                CurrentFPS = -1;
+            }
+            else
+            {
+                CurrentFPS = 1.0f / dt;
+            }
+        };
+
+        videoPlayer.started += (VideoPlayer) =>
+        {
+            timeOfLastFrame = Time.time;
+        };
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time - timeOfLastFrame > 3)
+        {
+            CurrentFPS = 0;
+        }
+    }
+}
+#pragma warning restore RECS0018 // Comparison of floating point numbers with equality operator
