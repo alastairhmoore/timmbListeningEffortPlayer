75ffac693564250c91911746c6bb0c4d21ab972e
Tim <timmb@matisse>
2019-08-15 11:02:15 +0100

    Add in basic pre-VR menu

---
+++ Assets/Scripts/MainMenu/StartButton.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class StartButton : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Button>().onClick.AddListener(() =>
+        {
+            SceneManager.LoadSceneAsync("MainScene");
+        });
+    }
+
+}
---
+++ Assets/Scripts/MainScene.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class MainScene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(LoadDevice());
+    }
+
+    IEnumerator LoadDevice()
+    {
+        XRSettings.LoadDeviceByName("cardboard");
+        yield return null;
+        XRSettings.enabled = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
