1c25f896c46e8f5f450ba1a9d14435e7ee7d188c
Tim <timmb@matisse>
2019-08-16 17:48:40 +0100

    more robust handling of download errors and repeat requests in video menu

--- Assets/Scripts/MainMenu/StartButton.cs
+++ Assets/Scripts/MainMenu/StartButton.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -6,13 +7,28 @@ using UnityEngine.UI;
 
 public class StartButton : MonoBehaviour
 {
+    private VideoSelectionUI[] uiElementsToCheck;
+
     // Start is called before the first frame update
     void Start()
     {
+        uiElementsToCheck = FindObjectsOfType<VideoSelectionUI>();
+        Debug.Log($"{uiElementsToCheck.Length} ui elements to check", this);
+        foreach (var elem in uiElementsToCheck)
+        {
+            elem.IsReadyChanged += (isReady, sender) =>
+            {
+                updateEnabled();
+            };
+        }
         GetComponent<Button>().onClick.AddListener(() =>
         {
             SceneManager.LoadSceneAsync("MainScene");
         });
     }
 
+    private void updateEnabled()
+    {
+        GetComponent<Button>().interactable = Array.TrueForAll(uiElementsToCheck, elem => elem.IsReady);
+    }
 }
--- Assets/Scripts/MainMenu/VideoSelectionUI.cs
+++ Assets/Scripts/MainMenu/VideoSelectionUI.cs
@@ -1,19 +1,43 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
 using UnityEngine.Networking;
 using UnityEngine.UI;
+using UnityEngine.Video;
 
 public class VideoSelectionUI : MonoBehaviour
 {
     public Text StatusText;
     public string VideoName;
-    private UnityWebRequest _request;
+    // for testing video
+    //private VideoPlayer _player;
+    private UnityWebRequest _mostRecentRequest;
+    private bool _isDownloading;
+    private VideoPlayer _player;
+
+    public event EventHandler<bool> IsReadyChanged;
+    /// <summary>
+    /// This indicates this video is downloaded and ready to go.
+    /// </summary>
+    private bool isReady;
+
+    public bool IsReady {
+        get => isReady;
+        private set {
+            isReady = value;
+            IsReadyChanged?.Invoke(this, isReady);
+        }
+    }
+
 
     // Start is called before the first frame update
     void Start()
     {
+        // player for testing videos
+        _player = gameObject.AddComponent<VideoPlayer>();
+
         string PrefsKey = $"{VideoName}_url)";
         InputField inputField = GetComponentInChildren<InputField>();
 
@@ -21,6 +45,7 @@ public class VideoSelectionUI : MonoBehaviour
         inputField.onEndEdit.AddListener((string url) =>
         {
             PlayerPrefs.SetString(PrefsKey, url);
+
             StartCoroutine(downloadVideo(url));
         });
 
@@ -34,38 +59,96 @@ public class VideoSelectionUI : MonoBehaviour
 
     IEnumerator downloadVideo(string url)
     {
-        StatusText.text = "Connecting...";
+        IsReady = false;
 
-        _request = new UnityWebRequest(url);
+        // To prevent overwriting of values, if there is already a request
+        // in process we need to stop it first
+        while (_mostRecentRequest != null)
+        {
+            _mostRecentRequest.Abort();
+            StatusText.text = "Cancelling previous download...";
+        }
+        IsReady = false;
+
+        StatusText.text = "Connecting...";
+        //var thisRequest = new UnityWebRequest(url);
+        //_mostRecentRequest = thisRequest;
+        _mostRecentRequest = new UnityWebRequest(url);
         string savePath = Path.Combine(Application.persistentDataPath, $"{VideoName}.mp4");
-        _request.downloadHandler = new DownloadHandlerFile(savePath)
+        _mostRecentRequest.downloadHandler = new DownloadHandlerFile(savePath)
         {
             removeFileOnAbort = true
         };
-        _request.timeout = 30;
-        yield return _request.SendWebRequest();
+        _mostRecentRequest.timeout = 30;
+        _isDownloading = true;
 
-        if (_request.isNetworkError || _request.isHttpError)
+        yield return _mostRecentRequest.SendWebRequest();
+
+        _isDownloading = false;
+
+        if (_mostRecentRequest.isNetworkError || _mostRecentRequest.isHttpError)
         {
-            StatusText.text = "Download error: " + _request.error;
-            _request = null;
+            // END OF THIS REQUEST
+            StatusText.text = "Download error: " + _mostRecentRequest.error;
+            _mostRecentRequest = null;
         }
         else
         {
-            Debug.Assert(_request.downloadHandler.isDone);
-            _request = null;
-            StatusText.text = "Download complete";
+            Debug.Assert(_mostRecentRequest.downloadHandler.isDone);
+            StatusText.text = "Checking video";
+
+            _player.url = savePath;
+            _player.source = VideoSource.Url;
+
+            _player.prepareCompleted += (source) =>
+            {
+                //// test that we haven't had a new request come in
+                //if (thisRequest == _mostRecentRequest)
+                //{
+                int width = _player.texture.width;
+                int height = _player.texture.height;
+                Debug.Log($"{VideoName} downloaded and of size {width}x{height}.");
+
+                bool isHD = width == 1920 && height == 1080;
+                bool is4K = width == 4096 && height == 2048;
+                if (!(isHD || is4K))
+                {
+                    StatusText.text = $"Video size is {width}x{height}. Should be 1920x1080 or 4096x2048.";
+                // END OF THIS REQUEST
+                    _mostRecentRequest = null;
+
+                }
+                else
+                {
+                    StatusText.text = $"Video downloaded successfully. Size: {width}x{height}.";
+                    IsReady = true;
+                    // END OF THIS REQUEST
+                    _mostRecentRequest = null;
+                }
+                //}
+            };
+
+            _player.errorReceived += (source, message) =>
+            {
+                StatusText.text = "Video error: " + message.Replace(savePath, url);
+                // END OF THIS REQUEST
+                _mostRecentRequest = null;
+            };
+
+            _player.Prepare();
+            // Request is not finished - will be completed by one of the above
+            // callbacks
         }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (_request != null)
+        if (_mostRecentRequest != null)
         {
-            if (_request.downloadProgress > 0)
+            if (_mostRecentRequest.downloadProgress > 0)
             {
-                StatusText.text = $"Downloading... {(int)(100 * _request.downloadProgress)}% complete";
+                StatusText.text = $"Downloading... {(int)(100 * _mostRecentRequest.downloadProgress)}% complete";
             }
         }
     }
